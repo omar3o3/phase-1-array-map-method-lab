3934ffa168913098e1bd6fb12763827d6e166f83
Omar <otantawy53@gmail.com>
2022-04-28 19:11:31 -0400

    commit tree 21a5a732a130db1c049b8b064efaaa9716711905

--- index.js
+++ index.js
@@ -12,5 +12,24 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  // return tutorials.map(item => item.split(' ').charAt(0).toUpperCase().join(' '));
+      //#1----original attempt^^^^ error is that 'charAt is not a function'
+  // return tutorials.map(item => item.split(' ')).charAt(0).toUpperCase().join(' ');
+      //#2----^^copy of original attempt but i moved the () around at the end -- error im getting is that 'char is not a function'
+  // return tutorials.map(item => item.split(' ').map(word => word.toUpperCase()).join(' '));
+      //#3----^^this attempt failed because it was returning every letter as capitlized
+  // return tutorials.map(item => item.split(' ').map(word => word.charAt(0).toUpperCase()).join(' '));
+      //#4----its only returning the capital letter of each word, no other letter
+  // return tutorials.map(item => item.split(' ').map(word => word.toUpperCase()).join(' '));
+      //#5----copy of attempt #3, messing around with where i put charAt
+  return tutorials.map(item => item.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' '));
+      //#6----copy of attempt #4, trying to figure out what happened to the other letters
+      //i got it after looking at the solution, i dont understand why i need slice
 }
+
+// const titleCased = () => {
+//   let newTutorials = tutorials.map(item => item.split(' '))
+//   return newTutorials;
+// }
+
+// console.log(titleCased);
